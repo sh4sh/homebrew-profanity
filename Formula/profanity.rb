11c4a004fb0253fd4d01abf5995b265f89e9ec04
Sunny Hashmi <6833405+sh4sh@users.noreply.github.com>
2024-12-11 18:25:27 -0500

    add profanity formula

---
+++ Formula/profanity.rb
@@ -0,0 +1,66 @@
+class Profanity < Formula
+  desc "Console based XMPP client"
+  homepage "https://profanity-im.github.io"
+  url "https://profanity-im.github.io/tarballs/profanity-0.14.0.tar.gz"
+  sha256 "fd23ffd38a31907974a680a3900c959e14d44e16f1fb7df2bdb7f6c67bd7cf7f"
+  license "GPL-3.0-or-later"
+
+  head do
+    url "https://github.com/profanity-im/profanity.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "autoconf-archive" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "libomemo-c" => :build
+  depends_on "pkgconf" => :build
+
+  depends_on "curl"
+  depends_on "glib"
+  depends_on "gnutls"
+  depends_on "gpgme"
+  depends_on "libgcrypt"
+  depends_on "libotr"
+  depends_on "libstrophe"
+  depends_on "python@3.13"
+  depends_on "readline"
+  depends_on "sqlite"
+
+  uses_from_macos "ncurses"
+
+  on_macos do
+    depends_on "gettext"
+    depends_on "libassuan"
+    depends_on "libgpg-error"
+    depends_on "terminal-notifier"
+  end
+
+  def install
+    ENV.prepend_path "PATH", Formula["python@3.13"].opt_libexec/"bin"
+
+    system "./bootstrap.sh" if build.head?
+
+    # We need to pass `BREW` to `configure` to make sure it can be found inside the sandbox in non-default
+    # prefixes. `configure` knows to check `/opt/homebrew` and `/usr/local`, but the sanitised build
+    # environment will prevent any other `brew` installations from being found.
+    system "./configure", "--disable-silent-rules",
+                          "--enable-python-plugins",
+                          "--enable-c-plugins",
+                          "--enable-plugins",
+                          "--enable-omemo",
+                          "--enable-omemo-qrcode",
+                          "--enable-otr",
+                          "--enable-pgp",
+                          "--enable-notifications",
+                          "BREW=#{HOMEBREW_BREW_FILE}",
+                          *std_configure_args
+    system "make", "install"
+  end
+
+  test do
+    system bin/"profanity", "-v"
+  end
+end
+
